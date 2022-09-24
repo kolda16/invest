553c7e28a7e2c899b247131791b7832f866a1069
Matej Kolar <50596718+kolda16@users.noreply.github.com>
2022-09-24 17:45:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,9 +12,15 @@ interest_rates_file = pandas.read_csv("int_rates.csv", index_col=None)
 date_lst = interest_rates_file.DATE.to_list()
 interest_rate_list = interest_rates_file.INTDSRUSM193N.to_list()
 
+interest_rate_chart_table_new = list()
+for date in date_lst:
+    date_chart = date[0:4]
+    interest_rate_chart_table_new.append(date_chart)
+
+
 interest_rate_chart_table = pandas.DataFrame(
     {'Interest Rate': interest_rate_list,
-     'Date': date_lst
+     'Date': interest_rate_chart_table_new
     })
 
 df = pandas.DataFrame(
@@ -24,5 +30,6 @@ df = pandas.DataFrame(
 
 st.dataframe(interest_rate_chart_table)
 st.line_chart(df)
+st.line_chart(interest_rate_chart_table)
 
 
