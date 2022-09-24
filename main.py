61aeb001e6d7176d492936017a5b46057edc4871
Matej Kolar <50596718+kolda16@users.noreply.github.com>
2022-09-24 16:48:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,17 @@
 import streamlit as st
 
 
-st.markdown("Understand your investments!")
+interest_rates_file = pandas.read_csv("/Users/Matej/Desktop/Stock/int_rates.csv", index_col=None)
+date_lst = interest_rates_file.DATE.to_list()
+interest_rate_list = interest_rates_file.INTDSRUSM193N.to_list()
 
-stock_name = st.text_input("Stock ticker")
-
-
-start_year, end_year = st.select_slider(
-    'Select a period!',
-    options=[2010, 2011, 2012, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020],
-    value=(2010, 2020))
-
-
-start_year = str(start_year)
-end_year = str(end_year)
-start_date = start_year+"-01-01"
-end_date = end_year+"-12-01"
-
-#stock_name = "AAPL"
-#start_date = "2015-01-01"
-#end_date = "2020-12-01"
-#request the stock ticker, which will be inputted into ir_bu script
-#stock_name = input("Input stock ticker:")
+interest_rate_chart_table = pandas.DataFrame(
+    {'Date': date_lst,
+     'Interest Rate': interest_rate_list,
+    })
 
 
-#after ir_bu script finishes, the code takes the output into this script
-if __name__ == "__main__":
-    from ir_bu import increase_table
-    st.dataframe(increase_table)
-    
+
+st.line_chart(interest_rate_chart_table)
 
 
